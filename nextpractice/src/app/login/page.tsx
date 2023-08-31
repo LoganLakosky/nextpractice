c4cd7cc2736048255407afbe954267c97099778b
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-08-31 08:32:47 -0500

    Worked on login and signup page

--- nextpractice/src/app/login/page.tsx
+++ nextpractice/src/app/login/page.tsx
@@ -1,10 +1,94 @@
+"use client";
+
 import "./login.css";
+
+import { ChangeEvent, useState } from "react";
+
 export default function Login() {
-  return <div className="mainLoginContainer">
-    <div className="mainLogin">
-      <div className="mainLoginTop">
-        <h1>Login</h1>
+  const [usernameLoginValue, setUsernameLoginValue] = useState<string>("");
+  const [passwordLoginValue, setPasswordLoginValue] = useState<string>("");
+
+  //Error states
+  const [usernameError, setUsernameError] = useState<boolean>(false);
+  const [passwordError, setPasswordError] = useState<boolean>(false);
+
+  function updateUsernameLoginValue(e: ChangeEvent<HTMLInputElement>) {
+    setUsernameLoginValue(e.target.value);
+  }
+
+  function updatePasswordLoginValue(e: ChangeEvent<HTMLInputElement>) {
+    setPasswordLoginValue(e.target.value);
+  }
+
+  async function logUserIn() {
+    if (usernameLoginValue === "") {
+      setTimeout(() => {
+        setUsernameError(false);
+      }, 1200);
+
+      setUsernameError(true);
+
+      return;
+    }
+
+    if (passwordLoginValue === "") {
+      // MAKE ERROR BETTER
+      alert("Please enter a password");
+      return;
+    }
+  }
+
+  return (
+    <div className="mainLoginContainer">
+      <div className="mainLogin">
+        <div className="mainLoginTop">
+          <h1>Login</h1>
+        </div>
+        <div className="mainLoginContentContainer">
+          <div className="mainLoginContent">
+            <div className="usernameInputContainer">
+              <div className="usernameWrapper">
+                {!usernameError && (
+                  <label htmlFor="username-label" className="usernameFirstLabel">
+                    Username
+                  </label>
+                )}
+
+                {usernameError && (
+                  <label htmlFor="username-label" style={{ color: "red", marginBottom: "4px" }}>
+                    enter a username
+                  </label>
+                )}
+
+                <input
+                  value={usernameLoginValue}
+                  type="text"
+                  id="username-label"
+                  onChange={updateUsernameLoginValue}
+                />
+              </div>
+            </div>
+            <div className="passwordInputContainer">
+              <div className="passwordWrapper">
+                <label htmlFor="password-label" className="passwordFirstLabel">
+                  Password
+                </label>
+                <input
+                  value={passwordLoginValue}
+                  type="text"
+                  id="password-label"
+                  onChange={updatePasswordLoginValue}
+                />
+              </div>
+            </div>
+            <div className="loginPageBtnContainer">
+              <button className="loginPageBtn" onClick={() => logUserIn()}>
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  );
 }
--- nextpractice/src/app/signup/page.tsx
+++ nextpractice/src/app/signup/page.tsx
@@ -6,6 +6,29 @@ export default function Signup() {
     <div className="mainSignupTop">
       <h1>Signup</h1>
     </div>
+    <div className="mainSignupContentContainer">
+          <div className="mainSignupContent">
+            <div className="usernameSignupContainer">
+              <div className="usernameWrapper">
+                <label htmlFor="username-label" className="usernameFirstLabel">
+                  Username
+                </label>
+                <input type="text" id="username-label" />
+              </div>
+            </div>
+            <div className="passwordSignupContainer">
+              <div className="passwordWrapper">
+                <label htmlFor="password-label" className="passwordFirstLabel">
+                  Password
+                </label>
+                <input type="text" id="password-label" />
+              </div>
+            </div>
+            <div className="signupPageBtnContainer">
+                <button className="signupPageBtn">Login</button>
+            </div>
+          </div>
+        </div>
   </div>
 </div>
 }
