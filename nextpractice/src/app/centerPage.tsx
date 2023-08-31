cf725afde89dff4fcd675f7c45c525f37744bae3
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-08-30 20:47:19 -0500

    Started mainContent component

--- nextpractice/src/app/centerPage.tsx
+++ nextpractice/src/app/centerPage.tsx
@@ -1,4 +1,6 @@
+import { Red_Hat_Display } from "next/font/google";
 import { useState, ChangeEvent } from "react";
+import MainContent from "./mainContent";
 
 type TodoType = {
   title: string;
@@ -9,6 +11,10 @@ export default function CenterPage() {
   const [todoTitleValue, setTodoTitleValue] = useState<string>("");
   const [todoBodyValue, setTodoBodyValue] = useState<string>("");
   const [todosArr, setTodosArr] = useState<TodoType[]>([]);
+
+  //Error states
+  const [todoTitleError, setTodoTitleError] = useState<boolean>(false);
+  const [todoBodyError, setTodoBodyError] = useState<boolean>(false);
 
   function updateTodoTitleValue(e: ChangeEvent<HTMLInputElement>) {
     setTodoTitleValue(e.target.value);
@@ -21,13 +27,23 @@ export default function CenterPage() {
   function createTodo() {
     if (todoTitleValue === "") {
       //ADD A BETTER ERROR MSG
-      alert("Todo Title cannot be empty");
+      setTimeout(() => {
+        setTodoTitleError(false);
+      }, 1200);
+
+      setTodoTitleError(true);
 
       return;
     }
+
     if (todoBodyValue === "") {
       //ADD A BETTER ERROR MSG
-      alert("Todo Body cannot be empty");
+      setTimeout(() => {
+        setTodoBodyError(false);
+      }, 1200);
+
+      setTodoBodyError(true);
+
       return;
     }
 
@@ -44,7 +60,12 @@ export default function CenterPage() {
       <div className="centerPageTopContainer">
         <div className="centerPageTop">
           <div className="todoTitleContainer">
-            <label htmlFor="todo-title">Title:</label>
+            {!todoTitleError && <label htmlFor="todo-title">Title</label>}
+            {todoTitleError && (
+              <label htmlFor="todo-title" style={{ color: "red", fontSize: "14px" }}>
+                Todo Title cannot be empty
+              </label>
+            )}
             <input
               value={todoTitleValue}
               type="text"
@@ -53,7 +74,13 @@ export default function CenterPage() {
             />
           </div>
           <div className="todoBodyInputContainer">
-            <label htmlFor="todo-body">Body:</label>
+            {!todoBodyError && <label htmlFor="todo-body">Body</label>}
+            {todoBodyError && (
+              <label htmlFor="todo-body" style={{ color: "red", fontSize: "14px" }}>
+                Todo Body cannot be empty
+              </label>
+            )}
+
             <input
               value={todoBodyValue}
               type="text"
@@ -61,15 +88,14 @@ export default function CenterPage() {
               onChange={updateTodoBodyValue}
             />
           </div>
+
           <div className="todoCreateBtnContainer">
             <button onClick={() => createTodo()}>Create Todo</button>
           </div>
         </div>
       </div>
       <div className="mainContentContainer">
-        {todosArr.map((todo) => {
-          return <h1 key={todo.body}>{todo.title}</h1>;
-        })}
+        <MainContent />
       </div>
     </div>
   );
