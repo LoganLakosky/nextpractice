d77e78d7c38d65d76cb7e92124bb7f8e69e7593b
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-09-01 16:27:53 -0500

    Added ability to add users to the database

--- nextpractice/src/app/navBar.tsx
+++ nextpractice/src/app/navBar.tsx
@@ -1,16 +1,18 @@
 import "./navBar.css";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div className="navBarContainer">
-      <div className="navBarLeft">
-        <h1>Tod's Todo's</h1>
+    <>
+      <div className="navBarContainer">
+        <div className="navBarLeft">
+          <h1>Tod's Todo's</h1>
+        </div>
+        <div className="navBarRight">
+          <Link href="/login">Login</Link>
+          <Link href="/signup">Signup</Link>
+        </div>
       </div>
-      <div className="navBarRight">
-        <Link href="/login">Login</Link>
-        <Link href="/signup">Signup</Link>
-      </div>
-    </div>
+    </>
   );
 }
--- nextpractice/src/app/signup/page.tsx
+++ nextpractice/src/app/signup/page.tsx
@@ -3,6 +3,10 @@ import { useState, ChangeEvent } from "react";
 
 import "./signup.css";
 
+import { initializeApp } from "firebase/app";
+import { getFirestore, setDoc, doc, getDoc, collection, getDocs } from "firebase/firestore";
+import { getDatabase, ref, set } from "firebase/database";
+
 export default function Signup() {
   const [usernameSignupValue, setUsernameSignupValue] = useState<string>("");
   const [passwordSignupValue, setPasswordSignupValue] = useState<string>("");
@@ -10,6 +14,50 @@ export default function Signup() {
   //Error states
   const [usernameError, setUsernameError] = useState<boolean>(false);
   const [passwordError, setPasswordErorr] = useState<boolean>(false);
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyC1xSf41NuZdXi9Ex-gM6LUqvF_PK0u1uI",
+    authDomain: "next-practice-f806e.firebaseapp.com",
+    projectId: "next-practice-f806e",
+    storageBucket: "next-practice-f806e.appspot.com",
+    messagingSenderId: "935446946378",
+    appId: "1:935446946378:web:772dad7adda9262d23bcb3",
+    measurementId: "G-5BTR9RJFC3",
+  };
+
+  const a = [1, 2, 3, 45, 56].reverse();
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
+  async function getAmountInQ() {
+    let docRef = doc(db, "peopleInQ", "totalInQ");
+    let docSnap = await getDoc(docRef);
+
+    let data = docSnap.data();
+    let inQ = data!.inQ;
+    let newestUserName = data!.name;
+
+    return [inQ, newestUserName];
+  }
+
+  async function postNewUser(userId: string, password: string) {
+    await setDoc(doc(db, "userInformation", userId), {
+      name: userId,
+      password: password,
+    });
+  }
+
+  async function getAllDocuments() {
+    const querySnapshot = await getDocs(collection(db, "userInformation"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  }
 
   function updateUsernameSignupValue(e: ChangeEvent<HTMLInputElement>) {
     setUsernameSignupValue(e.target.value);
@@ -35,6 +83,8 @@ export default function Signup() {
       setPasswordErorr(true);
       return;
     }
+
+    postNewUser(usernameSignupValue, passwordSignupValue);
   }
 
   return (
@@ -48,16 +98,16 @@ export default function Signup() {
             <div className="usernameSignupContainer">
               <div className="usernameWrapper">
                 {usernameError ? (
-                  <label htmlFor="username-label" className="usernameFirstLabel">
-                    Username
-                  </label>
-                ) : (
                   <label
                     htmlFor="username-label"
                     className="usernameFirstLabel"
                     style={{ color: "red" }}
                   >
                     Enter a username
+                  </label>
+                ) : (
+                  <label htmlFor="username-label" className="usernameFirstLabel">
+                    Username
                   </label>
                 )}
                 <input
@@ -71,16 +121,16 @@ export default function Signup() {
             <div className="passwordSignupContainer">
               <div className="passwordWrapper">
                 {passwordError ? (
-                  <label htmlFor="password-label" className="passwordFirstLabel">
-                    Password
-                  </label>
-                ) : (
                   <label
                     htmlFor="password-label"
                     className="passwordFirstLabel"
                     style={{ color: "red" }}
                   >
                     Enter a password
+                  </label>
+                ) : (
+                  <label htmlFor="password-label" className="passwordFirstLabel">
+                    Password
                   </label>
                 )}
                 <input
@@ -92,7 +142,9 @@ export default function Signup() {
               </div>
             </div>
             <div className="signupPageBtnContainer">
-              <button className="signupPageBtn">Signup</button>
+              <button className="signupPageBtn" onClick={() => signUserUp()}>
+                Signup
+              </button>
             </div>
           </div>
         </div>
